2c5f5a1ee8a36987a3b7f5a89abaa565fda4321f
frenchfrysays <lucas.e.fry@outlook.com>
2025-04-17 10:26:48 -0500

    Create dateFunction.js

---
+++ js/dateFunction.js
@@ -0,0 +1,7 @@
+function dateButton() {
+  var x = document.getElementById('dateButton');
+  var y = document.getElementById('date');
+  y.hidden = false;
+  x.hidden = true;
+  window.alert('p#date has been shown and button#dateButton has been hidden.');
+}
